a2353e4301bfe6ca5ffe09161e7c0ef6568b7b85
andres <andres.delgado@talentpitch.co>
2023-05-18 19:33:15 -0500

    finish config project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center text-center">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <h1>Portfolio</h1>
 </template>
