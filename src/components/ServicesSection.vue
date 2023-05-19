e3b08087e00cd053accdd510564c2fa15effaed9
andres <andres.delgado@talentpitch.co>
2023-05-19 16:27:13 -0500

    created services section

---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,58 @@
+<script>
+import { IconCode, IconDeviceDesktopCode } from '@tabler/icons-vue';
+
+export default {
+    components: { IconCode, IconDeviceDesktopCode }
+}
+</script>
+<template>
+    <div class="container mx-auto px-5 py-12">
+        <h6 class="text-white font-semibold pb-4 text-center">
+            My <span class="text-color-primary">Services</span>
+        </h6>
+        <h2 class="text-4xl text-white font-bold pb-5 text-center">
+            What I Do
+        </h2>
+        <div class="flex flex-col items-center justify-center lg:justify-between lg:flex-row">
+            <div class="w-full mt-6 bg-container-color px-6 py-12 rounded-lg border-2 border-[#21232c] hover:border-[#00AEFF] lg:mx-2 lg:mt-0 lg:w-1/3">
+                <div class="text-color-primary">
+                    <IconDeviceDesktopCode class="mx-auto" :size="60" stroke-width="2" />
+                </div>
+                <div class="py-4">
+                    <h3 class="text-center text-white font-semibold pb-5">
+                        Front-end developer
+                    </h3>
+                    <p class="text-body text-center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis labore fugiat officiis. Quam?
+                    </p>
+                </div>
+            </div>
+            <div class="w-full mt-6 bg-container-color px-6 py-12 rounded-lg border-2 border-[#21232c] hover:border-[#00AEFF] lg:mx-2 lg:mt-0 lg:w-1/3">
+                <div class="text-color-primary">
+                    <IconCode class="mx-auto" :size="60" stroke-width="2.5" />
+                </div>
+                <div class="py-4">
+                    <h3 class="text-center text-white font-semibold pb-5">
+                        Design UI
+                    </h3>
+                    <p class="text-body text-center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis labore fugiat officiis. Quam?
+                    </p>
+                </div>
+            </div>
+            <div class="w-full mt-6 bg-container-color px-6 py-12 rounded-lg border-2 border-[#21232c] hover:border-[#00AEFF] lg:mx-2 lg:mt-0 lg:w-1/3">
+                <div class="text-color-primary">
+                    <IconCode class="mx-auto" :size="60" stroke-width="2.5" />
+                </div>
+                <div class="py-4">
+                    <h3 class="text-center text-white font-semibold pb-5">
+                        Back-end developer
+                    </h3>
+                    <p class="text-body text-center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis labore fugiat officiis. Quam?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,11 @@
 import HeaderApp from '../components/layouts/HeaderApp.vue';
 import HomeSection from '../components/HomeSection.vue';
 import AboutSection from '../components/AboutSection.vue';
+import ServicesSection from '../components/ServicesSection.vue';
 
 
 export default {
-    components: { HeaderApp, HomeSection, AboutSection }
+    components: { HeaderApp, HomeSection, AboutSection, ServicesSection }
 }
 
 </script>
@@ -14,5 +15,6 @@ export default {
        <HeaderApp />
        <HomeSection />
        <AboutSection />
+       <ServicesSection />
     </div>
 </template>
